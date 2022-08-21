cbff5f63f0330e14cd638b104a0ef6d9a399ce6c
Luis Rodriguez <ljrzarate@gmail.com>
2022-08-21 17:50:28 -0500

    wtf

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,11 @@
 //= link_tree ../images
 //= link_tree ../builds
+//= link template/magnific-popup.css
+//= link template/jquery-ui.css
+//= link template/owl.carousel.min.css
+//= link template/owl.theme.default.min.css
+//= link template/lightgallery.min.css
+//= link template/bootstrap-datepicker.css
+//= link template/swiper.css
+//= link template/aos.css
 //= link template/style.css
