a552c961baad1e8c6c7eebe11eecfadf5273d113
Luis Rodriguez <ljrzarate@gmail.com>
2022-08-21 19:19:09 -0500

    single page wip

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "homes#index"
+  resource :home do
+    collection do
+      get :search
+    end
+  end
 end
