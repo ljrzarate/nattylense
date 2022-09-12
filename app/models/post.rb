0c7a8b862b9c4e487b825ab523dc307a6a554e73
srizzuto <srizzuto@gmail.com>
2022-09-12 14:01:22 -0300

    NL-05: Add seeds to populate tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  before_save :check_image_attachment
+
   has_one_attached :image do |attachable|
     attachable.variant :small, resize_to_limit: [400, 300]
     attachable.variant :big, resize_to_limit: [1280, 900]
@@ -7,5 +9,13 @@ class Post < ApplicationRecord
 
   def tag_names
     self.tag_list
+  end
+
+  private
+
+  def check_image_attachment
+    unless self.image.attached?
+      self.image.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'img_1.jpg')), filename: 'img_1.jpg', content_type: 'image/jpg')
+    end
   end
 end
